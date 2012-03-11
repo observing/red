dbd7d762bc42fc24bc393f7ce23ba4c48f3c520d
Arnout Kazemier <info@3rd-Eden.com>
2012-03-11 11:34:07 +0000

    Prevent all RED: events from the clients

--- tests/red.test.js
+++ tests/red.test.js
@@ -25,35 +25,35 @@ describe('RED.js', function () {
   describe('.Server', function () {
     describe('#http', function () {
       it('creates a new HTTP server instance', function () {
-        var server = RED.Server.HTTP({});
+        var red = RED.Server.HTTP({});
 
-        if (!(server instanceof require('http').Server)) {
+        if (!(red instanceof require('http').Server)) {
           should.fail('not a valid HTTP instance');
         }
       });
 
       it('configures a default request handler', function () {
-        var server = RED.Server.HTTP({})
-          , handler = server.listeners('request');
+        var red = RED.Server.HTTP({})
+          , handler = red.listeners('request');
 
         handler.should.have.length(1);
         handler.pop().should.be.a('function');
       });
 
       it('returns a uninitialized server', function (next) {
-        var server = RED.Server.HTTP({});
+        var red = RED.Server.HTTP({});
 
-        server.listen(TESTPORT, function (err) {
-          server.close();
+        red.listen(TESTPORT, function (err) {
+          red.close();
           next(err);
         });
       });
 
       it('returns a status 404 and noindex by default', function (next) {
         var port = TESTPORT
-          , server = RED.Server.HTTP({});
+          , red = RED.Server.HTTP({});
 
-        server.listen(port, function (err) {
+        red.listen(port, function (err) {
           if (err) return next(err);
 
           request.get('http://localhost:' + port, function (err, res, body) {
@@ -61,7 +61,7 @@ describe('RED.js', function () {
             res.headers['x-robots-tag'].indexOf('noindex').should.be.above(-1);
             res.headers['x-powered-by'].indexOf('RED').should.be.above(-1);
 
-            server.close();
+            red.close();
             next(err);
           });
         });
@@ -69,11 +69,11 @@ describe('RED.js', function () {
 
       it('can be configured for 302 redirects', function (next) {
         var port = TESTPORT
-          , server = RED.Server.HTTP({
+          , red = RED.Server.HTTP({
                 redirect: 'http://www.google.com/'
             });
 
-        server.listen(port, function (err) {
+        red.listen(port, function (err) {
           if (err) return next(err);
 
           request.get({
@@ -85,7 +85,7 @@ describe('RED.js', function () {
                 res.headers.location.should.equal('http://www.google.com/');
                 res.headers['x-powered-by'].indexOf('RED').should.be.above(-1);
 
-                server.close();
+                red.close();
                 next(err);
               }
           );
@@ -93,24 +93,24 @@ describe('RED.js', function () {
       });
 
       it('can also be configured for HTTPS', function () {
-        var server = RED.Server.HTTP({
+        var red = RED.Server.HTTP({
             key: require('fs').readFileSync(__dirname + '/ssl/red-key.pem')
           , cert: require('fs').readFileSync(__dirname + '/ssl/red-cert.pem')
         });
 
-        if (!(server instanceof require('https').Server)) {
+        if (!(red instanceof require('https').Server)) {
           should.fail('not a valid HTTPS instance');
         }
       });
 
       it('HTTPS server works the same as the same server', function (next) {
         var port = TESTPORT
-          , server = RED.Server.HTTP({
+          , red = RED.Server.HTTP({
                 key: require('fs').readFileSync(__dirname + '/ssl/red-key.pem')
               , cert: require('fs').readFileSync(__dirname + '/ssl/red-cert.pem')
             });
 
-        server.listen(port, function (err) {
+        red.listen(port, function (err) {
           if (err) return next(err);
 
           request.get('https://localhost:' + port, function (err, res, body) {
@@ -118,7 +118,7 @@ describe('RED.js', function () {
             res.headers['x-robots-tag'].indexOf('noindex').should.be.above(-1);
             res.headers['x-powered-by'].indexOf('RED').should.be.above(-1);
 
-            server.close();
+            red.close();
             next(err);
           });
         });
@@ -131,12 +131,157 @@ describe('RED.js', function () {
       });
 
       it('returns a Server instance', function () {
-        var server = RED.createServer();
+        var red = RED.createServer();
 
-        if (!(server instanceof RED.Server)) {
+        if (!(red instanceof RED.Server)) {
           should.fail('Not an RED.Server instance');
         }
       });
+    });
+  });
+});
+
+describe('RED', function () {
+  describe('~ constructing', function () {
+    it('can generate a RED server without additional options', function () {
+      RED.createServer();
+    });
+
+    it('accepts and applies an options argument', function () {
+      var red = RED.createServer({
+          namespace: 'foo'
+        , secret: 'hi'
+      });
+
+      red.namespace.should.equal('foo');
+    });
+  });
+
+  describe('#set', function () {
+    it('sets properties of RED', function () {
+      var red = RED.createServer();
+
+      red.namespace.should.equal('RED');
+      red.set('namespace', 'foo');
+
+      red.namespace.should.equal('foo');
+    });
+
+    it('only sets propertiest that already existed', function () {
+      var red = RED.createServer();
+
+      if (red.pewpew) should.fail('property already exited');
+      red.set('pewpew', 'pang');
+
+      if (red.pewpew) should.fail('should not do this');
+    });
+
+    it('should emit a event when you update a setting', function (next) {
+      var red = RED.createServer();
+
+      red.on('set:namespace', function namespacing (old, value, key) {
+        key.should.equal('namespace');
+        old.should.equal('RED');
+        value.should.equal('pew');
+
+        next();
+      });
+
+      red.set('namespace', 'pew');
+    });
+
+    it('should be chainable', function () {
+      var red = RED.createServer();
+
+      red.set('namespace', 'pew').should.equal(red);
+      red.set('pewpew', 'pang').should.equal(red);
+    });
+  });
+
+  describe('#configure', function () {
+    var env = process.env.NODE_ENV;
+
+    it('should always call the callback if no env argument is given', function (next) {
+      var red = RED.createServer();
+
+      red.configure(next);
+    });
+
+    it('sets the context of the function to `this`', function (next) {
+      var red = RED.createServer();
+
+      red.configure(function () {
+        this.should.equal(red);
+        next();
+      });
+    });
+
+    it('should only call the function if the env variable matches NODE_ENV', function (next) {
+      var red = RED.createServer()
+        , calls = 0;
+
+      function done () {
+        if (++calls >= 2) next();
+      }
+
+      red.configure('adfsdfsadfasdfasdfsdf', done);
+      red.configure(env, done);
+      red.configure(done);
+    });
+
+    it('should be chainable', function (next) {
+      var red = RED.createServer()
+        , calls = 0;
+
+      function done () {
+        if (++calls >= 3) next();
+      }
+
+      red.configure('adfsdfsadfasdfasdfsdf', done).should.equal(red);
+      red.configure(env, done).should.equal(red);
+      red.configure(done).should.equal(red);
+
+      // one extra done call, to ensure that all equal statements are executed
+      // before continue to the next test
+      done();
+    });
+  });
+
+  describe('#blacklisted', function () {
+    it('blacklists `error` by default', function () {
+      var red = RED.createServer();
+
+      red.blacklisted('error').should.equal(true);
+    });
+
+    it('blacklists all RED namespaced events by default', function () {
+      var red = RED.createServer();
+
+      red.blacklisted('RED:bananas').should.equal(true);
+    });
+
+    it('blacklists all namespace prefixed events by default', function () {
+      var red = RED.createServer({
+          namespace: 'foo'
+      });
+
+      red.blacklisted('foo:bananas').should.equal(true);
+    });
+
+    it('should use the configurable blacklist', function () {
+      var red = RED.createServer({
+          blacklist: ['foo', 'bar']
+      });
+
+      red.blacklisted('mew').should.equal(false);
+      red.blacklisted('foo').should.equal(true);
+      red.blacklisted('bar').should.equal(true);
+    });
+
+    it('doesnt blacklist other names', function () {
+      var red = RED.createServer();
+
+      red.blacklisted('bananas').should.equal(false);
     });
   });
 });
